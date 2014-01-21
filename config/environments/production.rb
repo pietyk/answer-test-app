c9e040d1517fe6de3519c7a00bc33fc7dc55c998
Lukasz Pietkiewicz <lukpie@relaximo.pl>
2014-01-21 12:29:43 +0100

    fixed memcached

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -40,7 +40,7 @@ AnswerTestApp::Application.configure do
   # config.logger = ActiveSupport::TaggedLogging.new(SyslogLogger.new)
 
   # Use a different cache store in production
-  config.cache_store = :dalli_store, ENV["MEMCACHEDCLOUD_SERVERS"].split(','), { :username => ENV["MEMCACHEDCLOUD_USERNAME"], :password => ENV["MEMCACHEDCLOUD_PASSWORD"] }
+  config.cache_store = :dalli_store, ENV["MEMCACHEDCLOUD_SERVERS"], { :username => ENV["MEMCACHEDCLOUD_USERNAME"], :password => ENV["MEMCACHEDCLOUD_PASSWORD"] }
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server
   # config.action_controller.asset_host = "http://assets.example.com"
