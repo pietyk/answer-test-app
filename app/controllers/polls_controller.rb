1d9e7784168019951d40422a7c82b83a411a5921
Lukasz Pietkiewicz <lukpie@relaximo.pl>
2014-01-22 23:03:56 +0100

    DRY get poll

--- app/controllers/polls_controller.rb
+++ app/controllers/polls_controller.rb
@@ -1,18 +1,22 @@
 class PollsController < ApplicationController
   before_filter :authenticate_user!
+  before_filter :get_poll
   
-  def show
-    Rails.logger.info("poll_#{params[:id].parameterize}")
-    @poll = Rails.cache.fetch("poll_#{params[:id]}") do
-      Poll.find(params[:id]) rescue nil
-    end
-    
+  def show   
     redirect_to root_path, :notice => I18n.t('poll_not_found') if @poll.nil?
   end
 
   def create
     params['answers'] = [] if params['answers'].nil?
-    @score            = Poll.find(params[:id]).generate_score(params['answers'].reject(&:empty?))
+    @score            = @poll.generate_score(params['answers'].reject(&:empty?))
     PollMail.send_score(current_user, @score).deliver
+  end
+  
+  private
+  
+  def get_poll
+    @poll = Rails.cache.fetch("poll_#{params[:id]}") do
+      Poll.find(params[:id]) rescue nil
+    end
   end
 end
